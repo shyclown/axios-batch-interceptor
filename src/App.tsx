1c2f6c6cc17e4c8c6447225756d5fc08305e88f1
Roman Moravcik <roman.moravcik1@gmail.com>
2021-11-05 18:24:32 +0100

    add button to run test from landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import runTest from "./test";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        <div
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={runTest}
+          style={{
+              cursor: 'pointer',
+              textDecoration: 'underline'
+          }}
         >
-          Learn React
-        </a>
+          Batch Request Files - test.ts
+        </div>
       </header>
     </div>
   );
